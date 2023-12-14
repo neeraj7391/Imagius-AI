81ec7e2fe4ab9294b8af45bb719c4980349d606a
Neeraj Keer <113324449+neeraj7391@users.noreply.github.com>
2023-12-14 23:18:02 +0530

    Add files via upload

---
+++ pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+import { Loader, Card, FormField } from '../components';
+
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+  
+  return (
+    <div>Home</div>
+  )
+}
+export default Home;
